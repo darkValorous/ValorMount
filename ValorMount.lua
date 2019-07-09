11b07c8b04d0f60bb5d9ad9e17317023309bcb8a
darkValorous <darkvalorous@twitch.tv>
2019-07-09 17:27:35 +1000

    Version increase

--- ValorMount.lua
+++ ValorMount.lua
@@ -6,7 +6,7 @@
 --------------------------------------------------------------------------------------------------
 local _G = _G
 local addonName = ...
-local vmVersion = "2.6"
+local vmVersion = "2.7"
 if not _G.ValorAddons then _G.ValorAddons = {} end
 _G.ValorAddons[addonName] = true
 
